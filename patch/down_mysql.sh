08ea2cccaa65405fb0ee629493b75db3501e6e19
NoYoWiFi <1284524409@qq.com>
2023-08-24 16:19:28 +0800

    2023-8-24 16:19:24

--- patch/down_mysql.sh
+++ patch/down_mysql.sh
@@ -3,16 +3,16 @@ VERSION=centos-${GV_VERSION}
 ADDRESS=registry.cn-shanghai.aliyuncs.com/zabbix_docker/zabbix_chinese
 for IM in `cat ./patch/.images_mysql`
 do
-echo "docker pull ${IM}_${VERSION}"
 docker pull ${ADDRESS}:${IM}_${VERSION}
 docker tag ${ADDRESS}:${IM}_${VERSION} ${IM}:${VERSION}
 docker rmi ${ADDRESS}:${IM}_${VERSION}
 done
-echo "docker pull centos_stream8"
 docker pull ${ADDRESS}:centos_stream8
 docker tag ${ADDRESS}:centos_stream8 quay.io/centos/centos:stream8
 docker rmi ${ADDRESS}:centos_stream8
-echo "docker pull 10.11.2"
 docker pull ${ADDRESS}:10.11.2
 docker tag ${ADDRESS}:10.11.2 mariadb:10.11.2
 docker rmi ${ADDRESS}:10.11.2
+docker pull ${ADDRESS}:grafana_grafana-oss
+docker tag ${ADDRESS}:grafana_grafana-oss grafana/grafana-oss:10.0.0
+docker rmi ${ADDRESS}:grafana_grafana-oss
